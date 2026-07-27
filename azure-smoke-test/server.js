617c11bc02f3550a6a7133e4ef8a1ef2e4e28e58
Erica Innocent Effiong <133363401+Ini-Quee@users.noreply.github.com>
2026-07-27 11:53:58 -0700

    Add Azure smoke test deployment

---
+++ azure-smoke-test/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.listen(port);
